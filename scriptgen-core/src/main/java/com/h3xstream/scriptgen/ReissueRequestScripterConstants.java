4c301b6e2e50efd515f85a05fa85a4a8eb1ee80b
h3xstream <h3xstr@gmail.com>
2014-06-08 11:24:47 -0400

    Preparing package for version 3 release. Update jar filename with version 3.

--- scriptgen-core/src/main/java/com/h3xstream/scriptgen/ReissueRequestScripterConstants.java
+++ scriptgen-core/src/main/java/com/h3xstream/scriptgen/ReissueRequestScripterConstants.java
@@ -2,7 +2,7 @@ package com.h3xstream.scriptgen;
 
 public class ReissueRequestScripterConstants {
 
-    public static String PLUGIN_NAME = "Reissue Request Scripter 2.0";
+    public static String PLUGIN_NAME = "Reissue Request Scripter 3.0";
     public static String AUTHOR = "Philippe Arteau (h3xstream)";
 
 }
