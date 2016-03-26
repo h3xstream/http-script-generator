d4891a5ad84bd988ae1d73e95ee654c5783b9caa
Philippe Arteau <parteau@gosecure.ca>
2016-03-26 00:32:28 -0400

    Update version in the title bar.

--- scriptgen-core/src/main/java/com/h3xstream/scriptgen/ReissueRequestScripterConstants.java
+++ scriptgen-core/src/main/java/com/h3xstream/scriptgen/ReissueRequestScripterConstants.java
@@ -2,7 +2,7 @@ package com.h3xstream.scriptgen;
 
 public class ReissueRequestScripterConstants {
 
-    public static String PLUGIN_NAME = "Reissue Request Scripter 4.0";
+    public static String PLUGIN_NAME = "Reissue Request Scripter 5.0";
     public static String AUTHOR = "Philippe Arteau (h3xstream)";
 
 }
